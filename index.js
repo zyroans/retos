65af34a1b1c9e7dfcc89f39979b44a2c4b69bc70
zyroans <103227897+zyroans@users.noreply.github.com>
2023-01-10 21:34:26 -0500

    first result

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 const giftsCities = [12, 3, 11, 5, 7, 17];
 const maxGifts = 20;
 const maxCities = 3;
+const results = document.createElement('div');
+appDiv.appendChild(results);
+results.innerHTML = createCube(3);
 
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
   let presentTotalWeight = giftsCities.reduce(
@@ -29,3 +32,36 @@ function getMaxGifts(giftsCities, maxGifts, maxCities) {
 }
 
 console.log(getMaxGifts(giftsCities, maxGifts, maxCities));
+
+function createCube(size) {
+  const newCube = [];
+  const numberOfLines = size * 2;
+  const topSide = '\\_';
+  const bottonSide = '_/';
+  const leftSide = '\\/';
+  const blankSpace = '&nbsp';
+  for (let i = 1; i <= numberOfLines; i++) {
+    if (i <= numberOfLines / 2) {
+      newCube.push(
+        blankSpace.repeat(numberOfLines / 2 - i) +
+          '/' +
+          leftSide.repeat(i - 1) +
+          topSide.repeat(size) +
+          '\\' +
+          '<br>'
+      );
+    }
+    if (i > numberOfLines / 2) {
+      console.log('paso la mitad ' + i);
+      newCube.push(
+        blankSpace.repeat(i - 1 - numberOfLines / 2) +
+          leftSide.repeat(numberOfLines - (i - 1)) +
+          bottonSide.repeat(size) +
+          '<br>'
+      );
+    }
+  }
+
+  return newCube.join('');
+}
+console.log(createCube(2));
